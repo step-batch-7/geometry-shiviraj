f6215453030f64e5c793ca0be9075b5e63010cdf
Shivam Rajput <58025059+shiviraj@users.noreply.github.com>
2019-12-14 11:22:10 +0530

    Add functionality of visit in Point class
    in visit function it will take a function reference
    and calculate the value according to that function

--- test/pointTest.js
+++ test/pointTest.js
@@ -9,4 +9,24 @@ describe('Point', () => {
       assert.deepStrictEqual(actual, '[Point @(2,3)]');
     });
   });
+
+  describe('visit', () => {
+    it('Should give the result according to function of sum', () => {
+      const point = new Point(2, 3);
+      const actual = point.visit((x, y) => x + y);
+      assert.deepStrictEqual(actual, 5);
+    });
+
+    it('Should give the result according to function of difference', () => {
+      const point = new Point(2, 3);
+      const actual = point.visit((x, y) => x - y);
+      assert.deepStrictEqual(actual, -1);
+    });
+
+    it('Should give the result according to function of product', () => {
+      const point = new Point(2, 3);
+      const actual = point.visit((x, y) => x * y);
+      assert.deepStrictEqual(actual, 6);
+    });
+  });
 });
